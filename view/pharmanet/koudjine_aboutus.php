e92fce1fcafc08456c99cb2e73f2beed5034e425
Onana Gael <lg.ngono@gmail.com>
2020-09-23 11:28:16 +0100

    merge

--- view/pharmanet/koudjine_aboutus.php
+++ view/pharmanet/koudjine_aboutus.php
@@ -1,6 +1,6 @@
 <?php
 
-$title_for_layout = ' Admin -' . 'Universités';
+$title_for_layout = ' ALSAS -' . 'Universités';
 $page_for_layout = ($position == 'Ajouter') ? 'Ajouter un assureur' : 'Modifier un assureur';
 $action_for_layout = 'Ajouter';
 
