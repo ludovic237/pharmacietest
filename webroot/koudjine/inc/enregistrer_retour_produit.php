49fc9668f997a214ef34a3ec8b8dd14b73feef25
Ludovic Ngono <lg.ngono@gmail.com>
2025-01-28 20:09:31 +0100

    mise a jour des quantites apres retour des produits (part 2)

--- webroot/koudjine/inc/enregistrer_retour_produit.php
+++ webroot/koudjine/inc/enregistrer_retour_produit.php
@@ -5,6 +5,7 @@ require_once('../Class/produit_retour.php');
 require_once('../Class/concerner.php');
 require_once('../Class/en_rayon.php');
 require_once('../Class/produit.php');
+require_once('../Class/vente.php');
 global $pdo;
 
 
@@ -13,6 +14,7 @@ $managerCa = new ProduitRetourManager($pdo);
 $managerConcerner = new ConcernerManager($pdo);
 $managerEnRayon = new En_rayonManager($pdo);
 $managerProduit = new ProduitManager($pdo);
+$managerVente = new VenteManager($pdo);
 
 $id = $_POST['id'];
 $qte = $_POST['qte'];
@@ -24,12 +26,25 @@ $retour = new ProduitRetour(array(
 ));
 $managerCa->add($retour);
 
+$qteTotalVente=0;
+$reductionParProduit=0;
+
 $concerner= $managerConcerner->get($id);
+$vente = $managerVente->get($concerner->vente_id());
+$en_rayon= $managerEnRayon->get($concerner->en_rayon_id());
+$produitr= $managerProduit->get($en_rayon->produit_id());
+
+$qteTotalVente=$concerner->quantite()+$qte;
+$reductionParProduit=$concerner->reduction()/$qteTotalVente;
+
 $concerner->setquantite($concerner->quantite()-$qte);
 echo "- concerne qte : ".$concerner->quantite();
 $managerConcerner->update($concerner);
 
-$en_rayon= $managerEnRayon->get($concerner->en_rayon_id());
+
+$vente->setprixTotal($vente->prixTotal()-(($en_rayon->prixVente()-$reductionParProduit)*$qte));
+$managerVente->update($vente);
+
 $en_rayon->setquantite($en_rayon->quantite()+$qte);
 echo "- rayon qte : ".$en_rayon->quantite();
 $en_rayon->setquantiteRestante($en_rayon->quantiteRestante()+$qte);
@@ -37,7 +52,6 @@ echo "- rayon qte res: ".$en_rayon->quantiteRestante();
 $managerEnRayon->update($en_rayon);
 
 
-$produitr= $managerProduit->get($en_rayon->produit_id());
 $produitr->setstock($produitr->stock()+$qte);
 echo "- produit stock : ".$produitr->stock();
 $managerProduit->update($produitr);
