1880c78834e7f42be227272ddb4cb6eac95acd29
Boris <bgomne@gmail.com>
2025-01-30 13:52:31 +0100

    annulation de la colonne quantite_retour dans la table concerner

--- requete sql 28-01-2025.sql
+++ requete sql 28-01-2025.sql
@@ -3,3 +3,14 @@
 
 ALTER TABLE `commande`
 ADD `employe_id` int NULL AFTER `id`;
+
+# ajout de la colonne dateEncaissement dans la table Vente
+
+ALTER TABLE `vente`
+ADD `dateEncaissement` datetime NULL AFTER `dateVente`;
+
+# selection des ID en_rayon avec _ 
+
+select * from en_rayon where id like '%\_%'
+
+update en_rayon set supprimer=1 where id like '%\_%'
