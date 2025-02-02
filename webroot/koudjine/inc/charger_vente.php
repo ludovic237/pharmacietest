622fb0851b65ae50b969c4882010e62022d108b2
Ludovic Ngono <lg.ngono@gmail.com>
2025-02-02 17:15:18 +0100

    mise a jour prix dans ticket de caisse (fix --2)

--- webroot/koudjine/inc/charger_vente.php
+++ webroot/koudjine/inc/charger_vente.php
@@ -130,7 +130,7 @@ if (isset($_POST['id'])) {
     $donnees = array(
         'data' => $data,
         "type_paiement" => $typefacturation,
-        'montantfactureEspece' => $montantfactureEspece,
+        'montantfactureEspece' => $montantfactureEspece+$ventes->reduction(),
         'montantfactureElectronique' => $montantfactureElectronique,
         'montantfactureTicket' => $montantfactureTicket,
         'reference' => $ventes->reference(),
